37601ef1cb06540ae618e9864595196cb0103d8c
Craster <704133029@qq.com>
2020-11-23 18:24:29 +0800

    ci: build in ci

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <HelloDing />
+</template>
+
+<script>
+import HelloDing from './components/HelloDing.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloDing,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 100%;
+  /* margin-top: 60px; */
+}
+</style>
---
+++ src/components/HelloDing.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>Ding1</h1>
+    <h2>Coming soon...</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloDing',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.hello {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #2c3e50;
+  color: #fff;
+}
+</style>
